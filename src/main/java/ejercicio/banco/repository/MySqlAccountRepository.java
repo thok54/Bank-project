219a5f1b8982cb0f6f98b20946113e90f3d47eb6
Jesus Alvarez Gonzalez <thok54@Jesuss-MacBook-Pro.local>
2019-06-24 17:04:03 +0200

    AccountRepositoryFix, though still weary of result sets

--- src/main/java/ejercicio/banco/repository/MySqlAccountRepository.java
+++ src/main/java/ejercicio/banco/repository/MySqlAccountRepository.java
@@ -45,53 +45,51 @@ public class MySqlAccountRepository implements AccountRepository {
 
     @Override
     public Account find(int id) {
-
         Connection con = dataBaseUtil.startConnection();
 
         try {
-            PreparedStatement pstmt = con.prepareStatement("select * from ACCOUNTS where id=" + id);
+            PreparedStatement pstmt = con.prepareStatement("select * from ACCOUNTS where id = " + id);
             ResultSet rs = pstmt.executeQuery();
-            if(rs.getInt("id")== id) {
+            if (rs.next()) {
                 String name = rs.getString("name");
                 Float money = rs.getFloat("money");
                 String iban = rs.getString("iban");
                 return new Account(id, name, money, iban);
             }
             throw new EntityNotFoundException(String.format("Account with ID = %d does not exist", id));
-    } catch(Exception e)
-
-    { e.printStackTrace();
-    } finally{
-        dataBaseUtil.closeConections(con);
-    }return null;
-}
-
-    @Override
-    public List<Account> findByName(String name) {
-        List<Account> accounts = new ArrayList();
-
-        Connection con = dataBaseUtil.startConnection();
-
-        try {
-            PreparedStatement pstmt = con.prepareStatement("select * from ACCOUNTS where name = " + name);
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                if (rs.getString("name") == name) {
-                    Integer id = rs.getInt("id");
-                    Float money = rs.getFloat("money");
-                    String iban = rs.getString("iban");
-                    Account account = new Account(id, name, money, iban);
-                    accounts.add(account);
-                }
-            }
-            return accounts;
-            //throw new EntityNotFoundException(String.format("Account with NAME = %s does not exist", name));
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             dataBaseUtil.closeConections(con);
         }
         return null;
+    }
+
+    @Override
+    public List<Account> findByName(String name) {
+        List<Account> accounts = new ArrayList();
+        Connection con = dataBaseUtil.startConnection();
+
+        try {
+            PreparedStatement pstmt = con.prepareStatement("select * from ACCOUNTS where name = " + name);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                while (rs.next()) {
+                    Integer id = rs.getInt("id");
+                    Float money = rs.getFloat("money");
+                    String iban = rs.getString("iban");
+                    Account account = new Account(id, name, money, iban);
+                    accounts.add(account);
+                }
+                return accounts;
+            }
+            throw new EntityNotFoundException(String.format("Account with NAME = %s does not exist", name));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            dataBaseUtil.closeConections(con);
+        }
+        return accounts;
     }
 
     @Override
